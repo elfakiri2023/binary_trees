9014e43cfdf2a954d18505dbf437d966ed00abb0
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-02-01 11:49:38 -0800

    Updating binary_trees.h

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_height - calculate the height the tree
+  * @tree: pointer
+  * Return: 0 if null or 1 if not
+  */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
+
+	if (left_height > right_height){
+		return (left_height);
+	}else{
+		return (right_height);
+	}
+}
