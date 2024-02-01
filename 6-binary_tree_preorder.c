d88e06efcf225f910cf93f8080b5e136f9c14b0d
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-02-01 08:52:08 -0800

    Updating binary_trees.h

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - binary tree using traversal pre-order.
+ * @tree: pointer
+ * @func: pointer
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+    if(!func)
+        return;
+
+	func(tree->n);
+
+    if (tree->left != NULL)
+	    binary_tree_preorder(tree->left, func);
+
+    if (tree->right != NULL)  
+	    binary_tree_preorder(tree->right, func);
+}
