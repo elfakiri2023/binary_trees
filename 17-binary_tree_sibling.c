430c9fdc6d773d6b830bbb69385b20865d00c415
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-02-01 16:11:46 -0800

    Updating 17-binary_tree_sibling.c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - search for the sibling
+ * @node: pointer
+ * Return: tree or NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+if (!node || !(node->parent) || !(
+node->parent->left) || !(node->parent->right))
+return (NULL);
+
+if (node == node->parent->left)
+return (node->parent->right);
+
+return (node->parent->left);
+}
